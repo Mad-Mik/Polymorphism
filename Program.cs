c8325dd0f937bdca3f2aad5df044491dcc3bd478
mikhail.madoyan <mikhail.madoyan@gmail.com>
2022-12-28 01:20:08 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Car
+    {
+        public virtual void Drive()
+        {
+            Console.WriteLine("I am drive");
+        }
+    }
+
+    class SportCar : Car
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("I am Drive Very Fast");
+        }
+
+    }
+
+    class Person
+    {
+        public void Drive(Car car)
+        {
+            car.Drive();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person person = new Person();
+
+            person.Drive(new SportCar()); //Methodi anuny nuynn e bayc gorcoxutyuny tarber
+        }
+    }
+}
